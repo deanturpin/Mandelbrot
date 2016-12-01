96603ecb6f32861a9b51ce5fffd6d3d2b57203b3
deanturpin <git@lvbtn.uk>
2016-12-01 12:42:17 +0000

    generating paths but only top left is being populated

--- buddhabrot/brot.js
+++ buddhabrot/brot.js
@@ -13,13 +13,15 @@ onload = function() {
 	var context = canvas.getContext("2d")
 
 	// Set canvas size
-	const width = 400 // window.innerWidth
-	const height = 300 // window.innerHeight
+	const width = 24 // window.innerWidth
+	const height = 24 // window.innerHeight
 
-	var zoom = 100000
-	const iterations = 20
+	var zoom = 150
+	const iterations = 3
 	canvas.width = width
 	canvas.height = height
+
+	// console.log = function() {}
 
 	// Create bitmap
 	var bitmap = new Array(width)
@@ -32,29 +34,54 @@ onload = function() {
 		for (var y = 0; y < height; ++y)
 			bitmap[x][y] = 0
 
-	// 'Brot limits
-	// var limit = {
-		// x: {min: -2, max: 2},
-		// y: {min: -1, max: 1},
-	// }
-
 	// Draw the 'brot
 	function brot() {
 
 		// Clear the canvas
 		context.clearRect(0, 0, canvas.width, canvas.height)
 
+		var maxIterations = 0
+
+		// Complex number class
+		function complex() {
+
+			// Real and imaginary
+			this.r
+			this.i
+
+			// Debug
+			this.print = function() { console.log(this.r, this.i) }
+
+			this.multiply = function(m) {
+
+				var p = new complex()
+				p.r *= m
+				p.i *= m
+
+				return p
+			}
+		}
+
 		// Test if point is a member of the set
-		function member(zr, zi, iterations) {
+		function calculateExitPath(zr, zi, iterations) {
+
+			var path = []
 
 			var cr = zr
 			var ci = zi
+
+			// Begin path
+			var p = new complex()
+			p.r = zr
+			p.i = zi
+
+			path[path.length] = p
 
 			for (var i = 0; i < iterations; ++i) {
 
 				// Don't look any further if we've escaped the set
 				if ((zr * zr + zi * zi) > 4)
-					return iterations - i
+					return path
 
 				// Calculate next point
 				const zr2 = (zr * zr) + (zi * zi * -1) + cr
@@ -65,25 +92,84 @@ onload = function() {
 				zi = zi2
 			}
 
-			return 0
+			// Return an empty array if we're in the set
+			return []
 		}
 
 		// Test if each element in the bitmap is a member of the set
 		for (var x = 0; x < width; ++x)
-			for (var y = 0; y < height; ++y)
-				bitmap[x][y] = member(
-					x/zoom - 1.9,
+			for (var y = 0; y < height; ++y) {
+
+				const path = calculateExitPath(
+					x/zoom - 2,
 					(y - height/2) / zoom,
 					iterations)
 
-		// Display 'brot
-		for (var x = 0; x < width; ++x)
-			for (var y = 0; y < height; ++y)
-				if (bitmap[x][y] > 0) {
+				// console.log(path.length)
 
-					context.fillStyle = "#f00"
+				if (path.length > 0)
+					for (var p = 0; p < path.length; ++p) {
+						
+						var point = path[p]
+						point.multiply(zoom)
+						point.r += 2
+
+						point.r = Math.round(point.r)
+						point.i = Math.round(point.i)
+
+						// point.print()
+
+						if (point.r < width && point.i < height
+							&& point.r >=0 && point.i >= 0) {
+
+							++bitmap[point.r][point.i]
+							// console.log("add", point.r, point.i, bitmap[point.r][point.i])
+						}
+						// else
+							// console.log("off screen", point.r, point.i)
+					}
+			}
+
+		// Calculate max intensity
+		var maxIntensity = 0
+		for (var x = 0; x < width; ++x)
+			for (var y = 0; y < height; ++y) {
+
+				// console.log("find max", x, y, bitmap[x][y], maxIntensity)
+
+				if (bitmap[x][y] > maxIntensity) {
+
+					maxIntensity = bitmap[x][y]
+					// console.log("bigger", maxIntensity)
+				}
+			}
+
+		console.log("max intensity", maxIntensity)
+
+		// Display 'brot
+		for (var x = 0; x < width; ++x) {
+
+			console.log(bitmap[x])
+
+			for (var y = 0; y < height; ++y) {
+
+				// console.log("display", x, y, bitmap[x][y])
+
+				if(bitmap[x][y] > 0) {
+
+					const s = Math.floor((bitmap[x][y]) * 256/maxIntensity) 
+
+					context.fillStyle = "rgb(" + s + ", " + s + ", " + s + ")"
+					// context.fillStyle = "white"
 					context.fillRect(x, y, 1, 1)
 				}
+				else {
+
+					context.fillStyle = "red"
+					context.fillRect(x, y, 1, 1)
+				}
+			}
+		}
 	}
 
 	var count = 0
